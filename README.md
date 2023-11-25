096a9733a32d5294695c470e9e58951910afd72b
Aya <aya.belarbi16@gmail.com>
2023-11-25 11:56:19 +0100

    finishing coding-challenge

--- README.md
+++ README.md
@@ -10,10 +10,42 @@ I utilized the REST Client extension in Visual Studio Code for API testing. The 
 
 ## How It Works
 
-### Setup
+### Setup - Algod node setting
 
-Installation of algokit in order to run an Algorand node. Some function requires to be connected to Algorand node in order to fetch some informations : account balances for instance. 
+To initiate the node, navigate to the `./sandbox` directory and execute the command `./sandbox up dev`. This sets up a local network.
 
-The ```accounts.js``` file is used to create accounts for API testing.
+Accounts are generated through the sandbox. To test the fluctuation of the account balance, transactions are sent from one account to another using the following command:
 
-I have installed as well the algokit folder in order to run up the Algorand blockchain : usefull to retrieve account information 
+`./sandbox goal clerk send -a 123456789 -f <account1> -t <account2>`
+
+Replace `<account1>` and `<account2>` with the addresses of the accounts you want to use for the transaction.
+
+### Test Accounts Configuration
+
+The sandbox network is configured with several pre-allocated accounts for testing. The configuration is defined in a JSON file (genesis.json) with the following structure:
+
+- `alloc`: An array of account objects, each with the following properties:
+  - `addr`: The address of the account.
+  - `comment`: A comment describing the purpose of the account.
+  - `state`: The initial state of the account, including the balance (`algo`), online status (`onl`), and other properties related to voting.
+
+Here are the test accounts:
+
+1. `RewardsPool`: An account with address `7777777777777777777777777777777777777777777777777774MSJUVU` and an initial balance of 100,000 Algos.
+
+2. `FeeSink`: An account with address `A7NMWS3NT3IUDMLVO26ULGXGIIOUQ3ND2TXSER6EBGRZNOBOUIQXHIBGDE` and an initial balance of 100,000 Algos.
+
+3. `Wallet1`: An account with address `N6CJ5SQBNGXE4P4PGUSQQEFHDJE6E33C7SLDJ354GYIDK5STIPMR3BLE6E` and an initial balance of 4,000,000,000,000,000 Algos.
+
+4. `Wallet2`: An account with address `BGW4JZMGQT56GCMK5ACT5MJP25SOYQEMU35KIOMFSKQTJTSIU3CCQEE2YQ` and an initial balance of 4,000,000,000,000,000 Algos.
+
+5. `Wallet3`: An account with address `JRYKGHGPKWHS6ICOWCIQPBM4KS4Z7S4MNG3NCYYXX727IC7UJFQESEWY4E` and an initial balance of 2,000,000,000,000,000 Algos.
+
+These accounts are used to test the fluctuation of account balances by sending transactions from one account to another.
+
+### Adding Funds, Removing Funds 
+
+The funds in the wallet are already present. To test whether the account state has changed or not, I send funds from one account to another. Then we will be able to see the changes. 
+
+```./sandbox goal clerk send -a 123456789 -f A7NMWS3NT3IUDMLVO26ULGXGIIOUQ3ND2TXSER6EBGRZNOBOUIQXHIBGDE -t JRYKGHGPKWHS6ICOWCIQPBM4KS4Z7S4MNG3NCYYXX727IC7UJFQESEWY4E
+ ```
--- api.js
+++ api.js
@@ -1,92 +1,99 @@
-const express = require('express');
-const algosdk = require('algosdk');
-const cors = require('cors');
+const express = require("express");
+const algosdk = require("algosdk");
+const cors = require("cors");
 const app = express();
 
-const token = 'a'.repeat(64);
+const token = "a".repeat(64);
 const port = 8080;
-const portAlgo = 4001; 
-const server = 'http://localhost';
+const portAlgo = 4001;
+const server = "http://localhost";
+
+const algodClient = new algosdk.Algodv2(token, server, portAlgo);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-
 });
 
 //Accepting an Algorand address
 let watcherList = [];
-app.post('/watcher', (req, res) => {
-   const address = req.body.address;
+app.post("/watcher", (req, res) => {
+  const address = req.body.address;
 
   try {
-    if(algosdk.isValidAddress(address)) {
+    if (algosdk.isValidAddress(address)) {
       watcherList.push(address);
-      res.send('Address added to watcher list');
+      res.send("Address added to watcher list");
     } else {
-      throw new Error('Invalid address');
+      throw new Error("Invalid address");
     }
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
-app.get('/watcher', (req, res) => {
+app.get("/watcher", (req, res) => {
   res.json(watcherList);
 });
 
 
+
 //Periodically checking the state of the accounts and logging notification
-let previousBalances = {};
+let lastKnownStates = {};
 function periodicStateCheck() {
-
   setInterval(async () => {
-    for (const address of watcherList) {
-      const algodClient = new algosdk.Algodv2(token, server, portAlgo);
-    
+    let i = 0; 
+    for (const address of watcherList ) {
+
       try {
-        const response = await algodClient.accountInformation(address).do(); 
-        const accountBalance = response.amount;
+        i++; 
+        const info = await algodClient.accountInformation(address).do();
+        const currentState = info;
 
-        console.log(`Account balance of ${address} : ${accountBalance} microAlgos`);
+        if (JSON.stringify(currentState) !== JSON.stringify(lastKnownStates[address])) {
+          console.log(`State of account ${address} has changed.`);
+          lastKnownStates[address] = currentState;
+        } else {
+          console.log(`No change in state of account ${address}.`);
+        }
 
-      // If the balance has changed since the last query, notify " new balance: ${accountBalance} microAlgos"
-      if (previousBalances[address] !== undefined && previousBalances[address] !== accountBalance) {
-        console.log(`New balance for account ${address}: ${accountBalance} microAlgos`);
+        // Check if the balance has changed
+       if (lastKnownStates[address] && lastKnownStates[address].amount !== currentState.amount) {
+        console.log(`New balance for account ${address}: ${currentState.amount} microAlgos`);
       }
 
-      // Update the previous balance
-      previousBalances[address] = accountBalance;
 
+      
+        // Update the previous balance
+        lastKnownStates[address] = currentState;
       } catch (error) {
         console.error(`Error checking state for account ${address}: ${error}`);
       }
     }
-  }, 6000); // 60 seconds interval
- }
+  }, 60000); // 60 seconds interval
+}
 periodicStateCheck();
 
 
+
 // Listing tracked accounts and their balances
-app.get('/accounts', async (req, res) => {
-  // Create an object to store the account information
-  let accountInfo = {};
+app.get("/accounts", async (req, res) => {
+  let accounts = [];
  
-  // Loop through the watcher list
   for (const address of watcherList) {
-    const algodClient = new algosdk.Algodv2(token, server, portAlgo);
- 
     try {
-      const response = await algodClient.accountInformation(address).do();
-      accountInfo[address] = response;
+      const info = await algodClient.accountInformation(address).do();
+      accounts.push({
+        address: address,
+        state: info
+      });
     } catch (error) {
-      console.error(`Error getting account information for address ${address}: ${error}`);
+      console.error(`Error checking state for account ${address}: ${error}`);
     }
   }
  
-  // Send the account information as a JSON response
-  res.json(accountInfo);
+  res.json(accounts);
  });
